f19186487b40cb0aa70066aa6e64fda4b571ce06
lufedev <luisferan738@gmail.com>
2023-12-11 14:20:52 -0500

    configuration-complete-rules-precommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Hola Lufedev</h1>
+      <h1 className='font-serif'>Hola Lufedev</h1>
     </main>
   )
 }
